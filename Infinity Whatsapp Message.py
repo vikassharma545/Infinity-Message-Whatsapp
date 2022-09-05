d53d71fc78d0b05c03920ecc10818bc5e80de870
Vikas Sharma <76645248+vikassharma545@users.noreply.github.com>
2022-09-05 18:08:10 +0530

    Update Infinity Whatsapp Message.py

--- Infinity Whatsapp Message.py
+++ Infinity Whatsapp Message.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 import os
 import sys
 import ctypes
@@ -40,10 +34,3 @@ while True:
     sleep(sleep_time)
     driver.find_element(By.XPATH, """//*[@id="side"]/div[1]/div/div/div[2]/div/div[2]""").send_keys(chat_name + Keys.ENTER)
     driver.find_element(By.XPATH, """//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]/p""").send_keys(msg + Keys.ENTER)
-
-
-# In[ ]:
-
-
-
-
